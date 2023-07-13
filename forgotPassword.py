ac0501b53ec74567a999f68e6917fbe161db7bf9
iambstha <iambstha@gmail.com>
2023-07-13 14:53:25 +0545

    email format check added

--- forgotPassword.py
+++ forgotPassword.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+import re
 opt = Options()
 opt.add_argument("--headless")
 
@@ -33,7 +34,18 @@ except NoSuchElementException:
 
 def askInfo():
     email = input("Enter the registered email: ")
-    action(email)
+    validate_email(email)
+    if validate_email(email):
+        print("Email is correct.")
+        action(email)
+    else:
+        print("Incorrect email format.")
+        askInfo()
+
+def validate_email(email):  
+    if re.match(r"[^@]+@[^@]+\.[^@]+", email):  
+        return True  
+    return False  
 
 def action(email):
     try:
@@ -48,4 +60,11 @@ def action(email):
             print("Submit button clicked.")
     except:
         print("Some error.")
+    
+    # try:
+    #     check_sent_status = driver.find_element(by=By.XPATH, value="<XPATH Here>")
+    #     if check_sent_status.text == 'Reset Link Sent In Email':
+    #         print("Success.") 
+    # except NoSuchElementException:
+    #     print('Error: Email doesnot exist.')
 askInfo()
