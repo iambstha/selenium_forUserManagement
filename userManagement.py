e2a593247d6a555abffbe612be14d4dc897bb6da
iambstha <iambstha@gmail.com>
2023-07-13 15:10:59 +0545

    user add testing

---
+++ userManagement.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+import re
+
+
+opt = Options()
+opt.add_argument("--headless")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opt)
+driver.implicitly_wait(5)
+
+getUrl = input("Enter the url: ")
+
+driver.get(getUrl)
+
+def validate_email(email):  
+    if re.match(r"[^@]+@[^@]+\.[^@]+", email):  
+        return True  
+    return False  
+
+try:
+    username_field = driver.find_element(by=By.XPATH, value="")
+    if username_field:
+        print("Username field found.")
+except NoSuchElementException:
+    print("Username field is not found.")
+
+try:
+    email_field = driver.find_element(by=By.XPATH, value="//input[@type='email']")
+    if email_field:
+        print("Email field found.")
+except NoSuchElementException:
+    print("Username field is not found.")
+
+try:
+    submit_button = driver.find_element(by=By.XPATH, value="//input[@type='submit']")
+    if submit_button:
+        print("Submit button found.")
+except NoSuchElementException:
+    print("Submit button not found.")
+
+def askInfo():
+    username = input("Enter the username: ")
+    email = input("Enter the email: ")
+    if validate_email(email):
+        action(username,email)
+    else:
+        print("Action error.")
+
+def action(username, email):
+    username_field.send_keys(username)
+    email_field.send_keys(email)
+    submit_button.click()
+    print("Added user succesfully.")
